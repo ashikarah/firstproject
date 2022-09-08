8d30bdc96316cf50ae54474c6d63e2de38896af9
ashikarah <110918097+ashikarah@users.noreply.github.com>
2022-09-08 19:27:23 +0530

    Update urls.py

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
      path('',views.add,name='add'),
      path('delete/<int:taskid>/', views.delete, name='delete'),
-     # path('update/<int:id>/', views.update, name='update'),
+     path('update/<int:id>/', views.update, name='update'),
      path('hhome/',views.tasklistview.as_view(),name='hhome'),
      path('detail/<int:pk>/',views.taskdetailview.as_view(),name='detail'),
      path('update/<int:pk>/',views.taskupdateview.as_view(),name='update'),
